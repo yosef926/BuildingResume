7d4e740d89f49b724dedd0eae7bb3aa4b85f62b4
yosef buznah <yosefx2000x@gmail.com>
2024-02-28 22:53:12 +0200

    endOfWorkDay - finished the welcome page, added a nice styled button that pass the user from the welcomePage to the collectFullname page

---
+++ my-app/src/mainPage/components/MainPage.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function MainPage() {
+    return (
+        <div className="wholePage">
+            <h1>hello</h1>
+        </div>
+    )
+}
--- my-app/src/welcomePage/components/WelcomePage.js
+++ my-app/src/welcomePage/components/WelcomePage.js
@@ -1,17 +1,26 @@
-import React from "react"
-import "../styles/welcomePage.css"
+import React from "react";
+import "../styles/welcomePage.css";
+import { useNavigate } from "react-router-dom";
+
 
 export default function WelcomePage() {
-    return (
-        <div className="wholePage">
-            <div className="welcomeLeft">
-                <h1 className="welcomeLeft--title">Welcome back</h1>
-                <h4 className="welcomeLeft--subTitle">Create your resume in just a few clicks</h4>
-                <img src={require("../images/resumeImg.png")} className="welcomeLeft--img" />
-            </div>
-            <div className="welcomeRight">
-                <h1 className="welcomeRight--title">Create resume in just a few minutes</h1>
-            </div>
-        </div>
-    )
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate("/register/CollectingFullName");
+  }
+
+  return (
+    <div className="wholePage">
+      <div className="welcomeLeft">
+        <h1 className="welcomeLeft--title">Welcome back</h1>
+        <h4 className="welcomeLeft--subTitle">Create your resume in just a few clicks</h4>
+        <img src={require("../images/resumeImg.png")} className="welcomeLeft--img" alt="resume" />
+      </div>
+      <div className="welcomeRight">
+        <h2 className="welcomeRight--title">Create resume in just a <br/>few minutes</h2>
+        <button className="button" onClick={handleClick}>START NOW</button>
+      </div>
+    </div>
+  );
 }
