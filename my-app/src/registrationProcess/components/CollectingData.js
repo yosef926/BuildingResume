15595e4eb11fd58e5a61ac912e5924e5e51e6719
yosef buznah <yosefx2000x@gmail.com>
2024-03-01 17:29:55 +0200

    Added background to the collectData Page, and some basic static input labels

--- my-app/src/registrationProcess/components/CollectingData.js
+++ my-app/src/registrationProcess/components/CollectingData.js
@@ -1,10 +1,36 @@
 import React from "react"
+import "../styles/collectData.css"
+
 
 export default function CollectingData(props) {
+
+    function handleSumbit() {
+        console.log("hello world")
+    }
+
+
     return (
-        <div>
-            <h1>Collecting data</h1>
-            <h1>this is the {props.page} page</h1>
+        <div className="root">
+            <img src={require("../images/background.png")} className="backgroundImg" />
+            <div className="userArea">
+                <h2>Enter your full name</h2>
+                <form onSubmit={handleSumbit}>
+                    <input
+                    type="text" 
+                    className="FirstName--inputBox"
+                    placeholder="First Name"
+                    />
+                    <input 
+                    type="text" 
+                    className="LastName--inputBox"
+                    placeholder="Last Name"
+                    />
+                </form>
+            </div>
         </div>
     )
 }
+
+
+
+// trying to understand how to keep the quality of the photo the same when im resizing it(height, width: 100%)
